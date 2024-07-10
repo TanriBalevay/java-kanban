686758c1ecf84ea7eef2341810f6ecceb165e35e
TanriBalevay <paramime12345@yandex.ru>
2024-07-11 01:59:12 +0300

    done4

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,4 @@
-import tracker.controllers.Managers;
-import tracker.controllers.TaskManager;
-import tracker.util.StatusTask;
-import tracker.model.Epic;
-import tracker.model.Subtask;
-import tracker.model.Task;
-import tracker.controllers.InMemoryTaskManager;
-
 public class Main {
-    private static int i, j;
     public static void main(String[] args) {
 
     }
