095f838d8bd8d38e972687ce4743992681e4a614
TanriBalevay <paramime12345@yandex.ru>
2024-11-12 23:22:00 +0300

    sprint 6.1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import tracker.controllers.InMemoryTaskManager;
 import tracker.model.Epic;
 import tracker.model.Subtask;
-import tracker.model.Task;
+
 import tracker.util.StatusTask;
 
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+
+
+
 public class Main {
     public static void main(String[] args) {
         InMemoryTaskManager taskManager = new InMemoryTaskManager();
