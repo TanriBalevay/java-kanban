6faed362d7c172a5e6e6e654167283a2d1d1461d
TanriBalevay <paramime12345@yandex.ru>
2024-11-25 01:06:09 +0300

    sprint 7 v1.4

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,3 @@
-import tracker.controllers.InMemoryTaskManager;
-import tracker.file.FileBackedTaskManager;
-import tracker.model.Epic;
-import tracker.model.Subtask;
-import tracker.model.Task;
-import tracker.util.StatusTask;
-import java.io.File;
-
-
 public class Main {
     public static void main(String[] args) {
 
